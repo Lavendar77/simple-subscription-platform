c610bb95882fd6ea3ac351e82183e43066ad4825
Adeyinka Adefolurin <folurinyinka@gmail.com>
2021-11-11 10:31:37 +0100

    cache the websites

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Website;
+use Illuminate\Support\Facades\Cache;
 
 class WebsiteController extends Controller
 {
@@ -13,7 +14,7 @@ class WebsiteController extends Controller
      */
     public function index()
     {
-        $websites = Website::all();
+        $websites = Cache::rememberForever('websites', fn () => Website::all());
 
         return response()->json([
             'status' => true,
